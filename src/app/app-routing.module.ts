2550a622a61dbe837ad44db865385b7bd17cf8d7
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-02-04 08:42:55 +0100

    kész a navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {path: '', redirectTo : 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'profil', component: ProfilComponent},
+  {path: 'services', component: ServicesComponent},
   {path: '**', redirectTo : 'home', pathMatch: 'full'}
   
 ];
